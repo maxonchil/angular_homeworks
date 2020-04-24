28e54fdbbad8dbfdb5b6990d489564cf0becc43f
max <maxonchil@gmail.com>
2020-04-24 20:06:25 +0300

    Homework1

--- homework1/src/app/start/start.component.spec.ts
+++ homework1/src/app/start/start.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { ClickerComponent } from "./clicker.component";
+import { StartComponent } from "./start.component";
 
-describe("ClickerComponent", () => {
-  let component: ClickerComponent;
-  let fixture: ComponentFixture<ClickerComponent>;
+describe("StartComponent", () => {
+  let component: StartComponent;
+  let fixture: ComponentFixture<StartComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ClickerComponent ]
+      declarations: [ StartComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ClickerComponent);
+    fixture = TestBed.createComponent(StartComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
